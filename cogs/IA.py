3b72415197b9ee02f2ac0d34daadcafd627f4f98
Bryan Felipe <bryanfelipech@gmail.com>
2025-10-09 00:10:10 -0300

    feat: modificando os modelos de IA

--- cogs/IA.py
+++ cogs/IA.py
@@ -29,9 +29,9 @@ INSTRUÇÕES DE COMPORTAMENTO ESSENCIAIS:
 class IA(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-       
-        self.text_model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-        self.image_model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+
+        self.text_model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+        self.image_model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
         self.conversations = {} # aqui é aonde é guardado todos os contextos das conversas com a IA, para resetar e apagar tudo, basta mencionar o bot e falar reset
         print("IA PRONTA!")
 
